2fbec4f872da5eaf200782037823a630d852d330
Mugen17 <aravindakumaran.v@gmail.com>
2016-11-14 22:11:01 +0530

    File Read

---
+++ Driver.java
@@ -0,0 +1,16 @@
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+public class Driver {
+   public static void main(String[] args){
+
+      try {	
+         SAXParserFactory factory = SAXParserFactory.newInstance();
+         SAXParser saxParser = factory.newSAXParser();
+         Handler userhandler = new Handler();
+         saxParser.parse("test.xml", userhandler);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }   
+}
